e931275b55a0e1703013fa7612f3650e040b1505
nadia arcese <nadiaarcese@gmail.com>
2019-09-09 14:25:40 -0400

    implemented switch statement

--- src/com/tss/dicegame/Game.java
+++ src/com/tss/dicegame/Game.java
@@ -2,50 +2,54 @@ package com.tss.dicegame;
 
 public class Game {
 
-    //rules
-
-//    //switch statement that sets rules for winning and losing
-
-    //        int outcome;
-//
-//    switch(outcome) {
-//
-//            case 7: case 11:
-//                System.out.println("WIN");
-//                break;
-//
-//            case 2: case 3: case 12:
-//                System.out.println("LOSE");
-//                break;
-//
-//            case 4:
-//                System.out.println("POINT is 4");
-//                break;
-//
-//            case 5:
-//                System.out.println("POINT is 5");
-//                break;
-//
-//            case 6:
-//                System.out.println("POINT is 6");
-//                break;
-//
-//            case 8:
-//                System.out.println("POINT is 8");
-//                break;
-//
-//            case 9:
-//                System.out.println("POINT is 9");
-//                break;
-//
-//            case 10:
-//                System.out.println("POINT is 10");
-//                break;
-//        }
     public static void main(String[] args) {
-
+        //roll each die and get value
         Dice dieOne = new Dice();
         Dice dieTwo = new Dice();
+
+        //variable represents value of one roll of the dice
+    int outcome;
+        outcome = dieOne.rollDie() + dieTwo.rollDie();
+
+//switch statement that sets rules for winning and losing
+        switch (outcome) {
+
+            case 7:
+            case 11:
+                System.out.println("WIN");
+                break;
+
+            case 2:
+            case 3:
+            case 12:
+                System.out.println("LOSE");
+                break;
+
+            case 4:
+                System.out.println("POINT is 4");
+                break;
+
+            case 5:
+                System.out.println("POINT is 5");
+                break;
+
+            case 6:
+                System.out.println("POINT is 6");
+                break;
+
+            case 8:
+                System.out.println("POINT is 8");
+                break;
+
+            case 9:
+                System.out.println("POINT is 9");
+                break;
+
+            case 10:
+                System.out.println("POINT is 10");
+                break;
+        }
+
     }
 }
 
